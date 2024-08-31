c3fc3d7345d05fc6adffb6fe16d74ecd34282621
edoscoba <obisimon86@gmail.com>
2024-08-31 15:13:44 +0100

    StakeERC20 contract

--- contracts/StakeERC20.sol
+++ contracts/StakeERC20.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+
+
 
  contract StakeERC20{ 
 
@@ -21,8 +24,16 @@ mapping(address => User) userStaking;
         uint rewardsEarned;
     }
 
-    function addStake(uint)external{
-      require();
+    function addStake(uint _stakedAmount, uint duration)external{
+      require(msg.sender != address(0), "address zero detected");
+      uint _userbalance = IERC20(tokenAddress).balanceOf(msg.sender);
+
+      require(_userbalance >= _stakedAmount,"You dont have enough token to stake" );
+
+      IERC20(tokenAddress).transferFrom(msg.sender, address(this), _stakedAmount);
+       balances[msg.sender] += _amount;
+
+
     }
 
     function CheckReward() external {}
@@ -30,6 +41,7 @@ mapping(address => User) userStaking;
     function checkEndTime() external {}
 
     function withdraw() external{
+      
       
     }
 }
